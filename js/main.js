38cca3c5527d34547a9d3c464b7f41cfbaf6f1e5
Jonatan Östling <jonatan.ostling@gmail.com>
2016-02-02 23:28:09 +0100

    Starting on the google api

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
-$('button').on('click', function() {
-  $('button').removeClass("selected");
-  $(this).addClass("selected");
+// $('button').on('click', function() {
+//   $('button').removeClass("selected");
+//   $(this).addClass("selected");
+//
+//   var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+//   $.getJSON(flickrAPI, flickrOptions, displayPhotos);
+//
+// });
 
-  $.ajax({
-    url: "http://brottsplatskartan.se/api.php?action=getEvents&period=1440&area=",
-    dataType: "json",
-    success: function(response) {
-      json = (response);
-      console.log(json);
-    }
-  });
+  // $.ajax({
+  //   url: "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+  //   dataType: "json",
+  //   success: function(response) {
+  //     json = (response);
+  //     console.log(json);
+  //   }
+  // });
 
-});
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    //prepare the request
+    var request = gapi.client.youtube.search.list({
+      part: "snippet",
+      type: "video",
+      q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+    })
+  })
+function init() {
+  gapi.client.setApiKey("AIzaSyDdKeKCAfv7oXA-opJJeQcGVPT6kOQSrNA");
+  gapi.client.load("youtube", "v3", function() {
+    //yt api is ready
+  })
+}
